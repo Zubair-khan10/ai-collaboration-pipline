b8391a9c19b48b9dbcae4f30e868077843b8c998
zubair khan <k92196612@gmail.com>
2026-08-17 00:38:03 +0500

    added ml model initialization

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import pandas as pd
 def load_data():
 print("data loading...")
 return pd.dataframe()
+fromsklearn.lonear-model import logisticRegression
+
+def init-model():
+#initializing model
+model = logisticRegression(max_iter=10)
+return model
